6f21243b788ce04a200733021b95d7b07432f232
francesco <francescoavitto88@gmail.com>
2024-01-21 19:15:16 +0100

    page contacts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,12 +1,23 @@
 <x-layout>
-    <div class="container my-5 py-5">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <h1>lorem ipsum dolor sit amet</h1>
+    <header class="bg-light pt-5">
+        <div class="bg-contacts min-vh-100 pt-5">
+          <div class="container h-100 py-0 py-md-5">
+            <div class="row h-100 align-items-center">
+                <aside class="col-12 col-md-6">
+                    <livewire:contact-form/>
+                </aside>
+                
             </div>
+          </div>
+      
+        </div>
+    </header>
+    {{-- <div class="container my-5 py-5">
+        <div class="row">
             <aside class="col-12 col-md-6">
+                <h1>lorem ipsum dolor sit amet</h1>
                 <livewire:contact-form/>
             </aside>
         </div>
-    </div>
+    </div> --}}
 </x-layout>
--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -5,33 +5,33 @@
     <fieldset>
         <form class="contact-form" wire:submit.prevent="sendContactMail">
             <div class="mb-3">
-                <input type="text" class="form-control" placeholder="NOME" wire:model="name">
+                <input type="text" class="form-control" placeholder="Nome" wire:model="name">
                 @error('name') <span class="error text-danger">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
-                <input type="text" class="form-control" placeholder="COGNOME" wire:model="surname">
+                <input type="text" class="form-control" placeholder="Cognome" wire:model="surname">
                 @error('surname') <span class="error text-danger">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
               <input type="email" class="form-control" 
                 id="exampleInputEmail1" aria-describedby="emailHelp"
-                 placeholder="EMAIL"
+                 placeholder="E-mail"
                  wire:model="email">
                  @error('email') <span class="error text-danger">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
-              <input type="phone" class="form-control" placeholder="TELEFONO" wire:model="phone">
+              <input type="phone" class="form-control" placeholder="Telefono" wire:model="phone">
               @error('phone') <span class="error text-danger">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
-                <textarea class="form-control" wire:model="message"></textarea>
+                <textarea class="form-control" wire:model="message">Scrivi qui il tuo messaggio...</textarea>
             </div>
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1" wire:model="terms">
-                <label class="form-check-label" for="exampleCheck1" >Check me out</label>
+                <label class="form-check-label" for="exampleCheck1" >Accetto termini e condizioni (GDPR) - Leggi qui</label>
                 @error('terms') <span class="error text-danger">{{ $message }}</span> @enderror
               </div>
-            <button type="submit" class="btn btn-primary w-100">Invia</button>
+            <button type="submit" class="btn submit-btn-form w-100">Invia</button>
           </form>
     </fieldset>
     @endif
